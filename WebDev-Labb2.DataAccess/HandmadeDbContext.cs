c5a34e0bb1070119059d4954af7c84408fa64cb0
Maria Edström <mariat.edstrom@gmail.com>
2024-03-14 14:03:27 +0100

    migration

--- WebDev-Labb2.DataAccess/HandmadeDbContext.cs
+++ WebDev-Labb2.DataAccess/HandmadeDbContext.cs
@@ -6,8 +6,8 @@ namespace WebDev_Labb2.DataAccess;
 
 public class HandmadeDbContext :DbContext 
 {
-    public DbSet<Product> Products { get; set; }
-    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; } 
+    public DbSet<Customer> Customers { get; set; } 
     public DbSet<Order> Orders { get; set; }
 
     public HandmadeDbContext(DbContextOptions options) : base(options)
---
+++ WebDev-Labb2.DataAccess/Migrations/20240314123704_grapes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDev_Labb2.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class grapes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
